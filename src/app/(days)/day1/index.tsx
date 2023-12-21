ebfec9fd588fb5d88d5ae8c169549e8ef89c113b
Susie Douang <susie@Susies-MacBook-Pro.local>
2023-12-21 13:32:36 -0600

    setup expo router

---
+++ src/app/(days)/day1/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const DaysDetailScreen = () => {
+  return (
+    <View>
+      <Text>DaysDetailScreen</Text>
+    </View>
+  );
+};
+
+export default DaysDetailScreen;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ import {
   Text,
   View,
 } from 'react-native';
-import DayListItem from './src/components/DayListItem';
+import DayListItem from '../components/DayListItem';
 import { useEffect } from 'react';
 
 import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
@@ -20,7 +20,7 @@ SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
-export default function App() {
+export default function HomeScreen() {
   const [fontsLoaded, fontError] = useFonts({
     Inter: Inter_900Black,
     Amatic: AmaticSC_400Regular,
